ddf1e3fe71230a358e2b748e4de91bab7fe49d87
Nguyen Thanh Trung <trunghy2k4111@gmail.com>
2025-03-08 18:41:27 +0700

    Làm thêm ui phần TopUser

--- front-end/src/pages/TopUsers.jsx
+++ front-end/src/pages/TopUsers.jsx
@@ -1,7 +1,14 @@
+import TableUsers from "../components/UI/TableUser";
 const TopUsers=()=>{
     return(<>
-        <h1>Page top User</h1>
-    
+    <div className="container mt-5">
+        <div className="row">
+                <div className="mb-4 col-lg-6"> <TableUsers/></div>
+                <div className="mb-4 col-lg-6"> <TableUsers/></div>
+        </div>
+
+    </div>
+           
     </>);
 }
 export default TopUsers;
