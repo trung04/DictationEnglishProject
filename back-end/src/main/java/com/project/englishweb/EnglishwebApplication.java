0e8c41c0258db828fe8b1cfd64b7aee610e83875
Nguyen Thanh Trung <trunghy2k4111@gmail.com>
2025-03-16 17:36:55 +0700

    thêm backend

---
+++ back-end/src/main/java/com/project/englishweb/EnglishwebApplication.java
@@ -0,0 +1,13 @@
+package com.project.englishweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishwebApplication.class, args);
+	}
+
+}
