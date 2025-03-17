5731e6da9c5a1f8fe2993805fbaa8979cccc89e1
Nguyen Thanh Trung <trunghy2k4111@gmail.com>
2025-03-17 11:52:45 +0700

    thêm giao diện layout admin

---
+++ back-end/src/main/java/com/project/englishweb/controllers/admin/AdminController.java
@@ -0,0 +1,16 @@
+package com.project.englishweb.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+    @GetMapping("/layout")
+    public String layout(){
+        return "layout/layout";
+    }
+}
