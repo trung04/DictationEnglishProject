0cd1ce70559a15af88f455b6e200c6d1c63e3c81
NguyenDacHoang08 <dachoang2k4@gmail.com>
2025-03-21 23:36:14 +0700

    dieu chinh

--- back-end/src/main/java/com/project/englishweb/Entity/Comment.java
+++ back-end/src/main/java/com/project/englishweb/Entity/Comment.java
@@ -14,7 +14,7 @@ public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long commentId;
 
     @Column(nullable = false)
     private String content;
