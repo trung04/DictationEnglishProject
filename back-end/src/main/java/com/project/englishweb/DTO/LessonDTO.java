abe2bf6bd269b7c54b4927d6cab625620bbc1c88
akamanto123 <143337546+akamanto123@users.noreply.github.com>
2025-03-28 19:14:41 +0700

    Add files via upload

---
+++ back-end/src/main/java/com/project/englishweb/DTO/LessonDTO.java
@@ -0,0 +1,13 @@
+package com.project.englishweb.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LessonDTO {
+    private String title;
+    private String level;
+    private String URL;
+    private Long topicId;
+}
